0a7bbb6a2e619bcf46c0265680b78bf01f96f0b4
termer <termer@termer.net>
2022-12-25 20:56:13 -0500

    create annotations overlay

--- player.js
+++ player.js
@@ -1,23 +1,136 @@
+'use strict'
+
+const _html5VideoAnnotationsStyle = document.createElement('style')
+_html5VideoAnnotationsStyle.innerText = `
+.html5-video-annotations-overlay {
+	background: none;
+	position: absolute;
+	overflow: visible;
+	pointer-events: none;
+}
+.html5-video-annotations-overlay * {
+	pointer-events: auto;
+	position: absolute;
+}
+`
+document.head.appendChild(_html5VideoAnnotationsStyle)
+
 /**
  * @typedef AnnotationPlayerOptions
  * Instantiation options for AnnotationPlayer
- * @property {number?} fallbackZIndex The z-index to use when an appropriate one cannot be determined
+ * @property {number?} defaultZIndex The default CSS z-index value to use for the overlay if the <video> element does not have an assigned z-index
+ * @property {(HTMLElement | null)?} proxyEventsTo When the annotations overlay generates an event, its events will be proxied to this element, or null for none
  */
 
 /**
  * A video annotation player instance
  */
 class AnnotationPlayer {
+	/* Properties */
+	/** @type {HTMLDivElement} */
+	#overlay
+	/** @type {HTMLVideoElement} */
+	#video
+	/** @type {AnnotationPlayerOptions} */
+	#options
+	/** @type {number} */
+	#resizeInterval
+
+	/* Getters */
 	/**
-	 * @param {HTMLVideoElement} videoElement The <video> element to bind to
+	 * The <div> overlay that contains annotation content
+	 * @type {HTMLDivElement}
+	 */
+	get overlay() { return this.#overlay }
+
+	/**
+	 * The <video> element
+	 * @returns {HTMLVideoElement}
+	 */
+	get video() { return this.#video }
+
+	/**
+	 * Returns whether the annotations overlay is visible
+	 * @returns {boolean}
+	 */
+	get isVisible() { return this.#overlay.style.display !== 'none' }
+
+	/**
+	 * @param {HTMLVideoElement} video The <video> element
 	 * @param {AnnotationPlayerOptions?} options
 	 */
-	constructor(videoElement, options) {
+	constructor(video, options) {
+		// Resolve options
 		/** @type {AnnotationPlayerOptions} */
 		const defaultOptions = {
-			fallbackZIndex: 100
+			defaultZIndex: 100,
+			proxyEventsTo: null
 		}
-
+		/** @type {AnnotationPlayerOptions} */
 		const ops = { ...defaultOptions, ...options }
+
+		// Create and set up overlay
+		const overlay = document.createElement('div')
+		overlay.classList.add('html5-video-annotations-overlay')
+		overlay.style.display = 'block'
+
+		// Update display periodically and on relevant events
+		const updateDisplayCallback = this.updateOverlayDisplay.bind(this)
+		video.parentElement.addEventListener('fullscreenchange', updateDisplayCallback)
+		video.addEventListener('resize', updateDisplayCallback)
+		this.#resizeInterval = setInterval(updateDisplayCallback, 1000) // <-- Every second
+
+		// Set properties
+		this.#video = video
+		this.#options = ops
+		this.#overlay = overlay
+
+		// Append overlay and update display
+		video.parentElement.append(overlay)
+		this.updateOverlayDisplay()
+	}
+
+	/**
+	 * Updates the overlay's size and coordinates on the screen to cover its associated <video> element
+	 */
+	updateOverlayDisplay() {
+		const video = this.#video
+		const overlay = this.#overlay
+
+		// Calculate the video's z-index
+		const zIndexStr = getComputedStyle(video).zIndex
+		const overlayZIndex = zIndexStr === 'auto' ? this.#options.defaultZIndex : parseInt(zIndexStr) + 1
+
+		// Update overlay properties
+		overlay.style.zIndex = overlayZIndex.toString()
+		overlay.style.width = `${video.clientWidth}px`
+		overlay.style.height = `${video.clientHeight}px`
+		overlay.style.left = `${video.offsetLeft}px`
+		overlay.style.top = `${video.offsetTop}px`
+	}
+
+	/**
+	 * Hides the annotations overlay
+	 */
+	hide() {
+		this.#overlay.style.display = 'none'
+	}
+
+	/**
+	 * Shows the annotations overlay
+	 */
+	show() {
+		this.#overlay.style.display = 'block'
+	}
+
+	/**
+	 * Toggles the annotations overlay's visibility
+	 */
+	toggleVisibility() {
+		if (this.isVisible) {
+			this.hide()
+		} else {
+			this.show()
+		}
 	}
 }
